55807552c9888fa80e189cd9a6e776781c93f6ca
VempatiSiva456 <vempati.reddy@students.iiit.ac.in>
2025-01-09 10:43:36 +0530

    changed api url

--- roast-page/src/api.js
+++ roast-page/src/api.js
@@ -1,5 +1,5 @@
 import { v4 as uuidv4 } from "uuid";
-const API_URL = "https://profile-roaster.onrender.com/api";
+const API_URL = "https://linkedin-roaster.onrender.com/api";
 
 const getDeviceId = () => {
     let deviceId = localStorage.getItem("deviceId");
