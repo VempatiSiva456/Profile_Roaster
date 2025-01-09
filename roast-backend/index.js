24f235359885fc855070d50ae93b53380a67418c
VempatiSiva456 <vempati.reddy@students.iiit.ac.in>
2025-01-09 10:36:05 +0530

    connected client build folder in server

--- roast-backend/index.js
+++ roast-backend/index.js
@@ -10,8 +10,13 @@ connectDB();
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.static('../roast-page/dist'));
 
 app.use(roastRoutes);
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../roast-page/dist', 'index.html'));
+});
 
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
